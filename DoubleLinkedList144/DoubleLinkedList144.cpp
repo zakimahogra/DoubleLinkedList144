b6d1e95e3c18100ea9840838818fc6fd74d1a134
M S I <M S I@MSI>
2024-05-04 21:09:21 +0700

    deklarasi fungsi search

--- DoubleLinkedList144/DoubleLinkedList144.cpp
+++ DoubleLinkedList144/DoubleLinkedList144.cpp
@@ -64,3 +64,18 @@ void addNode()
 	}
 }
 
+bool search(int rollNo, Node** previous, Node** current)
+
+{
+
+	*previous = NULL;
+	*current = START;
+	while (*current != NULL && (*current)->noMhs != rollNo)
+
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
+}
+
