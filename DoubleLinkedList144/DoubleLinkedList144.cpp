13549f3a0b4725711ae93a5b9c6f5945ca0cc51e
M S I <M S I@MSI>
2024-05-04 21:11:05 +0700

    membuat start dari first node

--- DoubleLinkedList144/DoubleLinkedList144.cpp
+++ DoubleLinkedList144/DoubleLinkedList144.cpp
@@ -93,4 +93,19 @@ void deleteNode()
 		return;
 	}
 
+	current = START;
+	previous = NULL;
+
+	while (current != NULL && current->noMhs != rollNo)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		cout << "\033[31mThe record with roll number " << rollNo << "not found\033[0m" << endl;
+		return;
+	}
+
 	
