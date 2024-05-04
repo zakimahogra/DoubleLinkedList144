faf1511093c17619e0b7f43c4b73e6cc45961087
M S I <M S I@MSI>
2024-05-04 21:14:40 +0700

    membuat conditional statement

--- DoubleLinkedList144/DoubleLinkedList144.cpp
+++ DoubleLinkedList144/DoubleLinkedList144.cpp
@@ -191,3 +191,51 @@ void searchData()
 	}
 }
 
+int main()
+{
+	while (true)
+	{
+		try
+		{
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all records in the ascending order of roll numbers" << endl;
+			cout << "4. View all records in the descending order of roll numbers" << endl;
+			cout << "5. search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter your choice (1-6): " << endl;
+			char ch;
+			cin >> ch;
+
+			switch (ch)
+			{
+			case '1':
+				addNode();
+				break;
+			case '2':
+				deleteNode();
+				break;
+			case '3':
+				traverse();
+				break;
+			case '4':
+				revtraverse();
+				break;
+			case '5':
+				searchData();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
+			}
+
+		}
+		catch (exception& e)
+		{
+			cout << "Check for the values entered." << endl;
+		}
+	}
+}
