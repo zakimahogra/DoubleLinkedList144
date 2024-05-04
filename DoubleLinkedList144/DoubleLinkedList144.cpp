6da5f78964cbb6226adb5fc1974c489a1ef6a822
M S I <M S I@MSI>
2024-05-04 21:13:43 +0700

    deklarasi fungsi retraverse

--- DoubleLinkedList144/DoubleLinkedList144.cpp
+++ DoubleLinkedList144/DoubleLinkedList144.cpp
@@ -151,3 +151,22 @@ void traverse()
 	}
 }
 
+void revtraverse()
+{
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else
+	{
+		cout << "/nRecords in descending order or roll number are: " << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
+
